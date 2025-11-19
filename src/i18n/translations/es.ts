f7814db7d8bf1233e60eb9e3009ec620706ad7f8
JimmyBase <j.tordeur@camarafuerteventura.org>
2025-11-19 09:20:15 +0000

    Updated ProgramPage.tsx

--- src/i18n/translations/es.ts
+++ src/i18n/translations/es.ts
@@ -309,7 +309,7 @@ export default {
                 description: 'Cóctel de bienvenida para los asistentes del evento. Tendrá lugar en el Hotel Sheraton Fuerteventura Beach, Golf & Spa Resort, Av. de las Marismas, 1, 35610 Antigua, Las Palmas. En el Salón Tenerife a las 20:45 horas.',
                 location: 'Hotel Sheraton Fuerteventura Beach, Golf & Spa Resort',
                 locationAddress: 'Av. de las Marismas, 1, 35610 Antigua, Las Palmas',
-                mapLink: 'https://www.google.com/maps/place/Sheraton+Fuerteventura+Beach,+Golf+%26+Spa+Resort/@28.3872431,-13.8686424,17z/data=!3m1!4b1!4m9!3m8!1s0xc46bfdb9a5c6c5d:0x8c9e1c9e1c9e1c9e!5m2!4m1!1i2!8m2!3d28.3872431!4d-13.8660675!16s%2Fg%2F1tdxr3l1?entry=ttu'
+                mapLink: 'https://maps.app.goo.gl/3qrMjsbEUPfSAHa57'
               }
             ]
           }
